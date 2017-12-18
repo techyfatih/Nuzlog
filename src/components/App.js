b5978d29070d7fd11371f235a7927ab7043ce5e4
Fatih Ridha <abgfatih@gmail.com>
2017-12-18 10:31:34 -0800

    v.2.0.3

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header version='2.0.2' />
+        <Header version='2.0.3' />
         <Body />
         <Footer />
       </div>
